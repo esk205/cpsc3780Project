9a54730782f7148acd96b008f5d5868e341f1d47
carlyhexo <95105215+carlyhexo@users.noreply.github.com>
2021-11-26 14:13:01 -0700

    Add files via upload

---
+++ bouncer.cc
@@ -0,0 +1,248 @@
+// Project assignment 3 solution; compile with C++11.
+// Source: Beej's Guide to Network Programming examples: listener.c and talker.c
+// DESIGN:
+// uses 2 sockets, one for receiving, used by one thread; one for sending, used by the other thread
+// The sender sends a UDP packet every second. Waiting is performed on a condition variable, so the thread can wake up if the receiver receives something. The sender sends a single byte as payload, taken from the global value field.
+// The receiver changes this global value upon receipt of a packet; it then notifies the sender who will send a new packet using the updated value.
+
+
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <mutex>
+#include <condition_variable>
+#include <cstdlib>
+#include <ctime>
+#include <cerrno>
+#include <cstring>
+
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+#define MYPORT "5010"	// the port on which we receive
+#define DESTPORT "5000"  // the port where we send packets
+
+/* Cmd line arguments: bouncer destination_addr */
+
+#define MAXBUFLEN 100
+
+/* Since we use threads, we declare some global variables first. 
+It works just fine to declare the globals outside of a struct, but gathered in a struct just feels you are more in control :) 
+*/
+
+struct globals_t {
+  unsigned char value='a';  // the byte value being sent
+  std::condition_variable cv_timer;  // for the timed wait
+  // protect concurrent access by the other thread; 
+  std::mutex exclude_other_mtx;  
+  
+};
+
+struct globals_t gl;
+
+// get sockaddr, IPv4 or IPv6: 
+void *get_in_addr(struct sockaddr *sa)
+{
+	if (sa->sa_family == AF_INET) {
+		return &(((struct sockaddr_in*)sa)->sin_addr);
+	}
+
+	return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+
+// creates the socket for receiving, using family to identify the protocol and port for the port we will be receiving on
+// family = AF_INET or AF_INET6 or AF_UNSPEC
+// port = string representing the port number we bind to as receiver
+// return
+//   -1 on error
+//   the socked descriptor if OK
+int create_sock_recv(int family, const char * port) {
+  struct addrinfo hints, *servinfo, *p;
+  int rv;
+
+  int the_sock;
+  
+    // start with the hints (local address, UDP, etc
+  memset(&hints, 0, sizeof hints);
+  hints.ai_family = family; // set to AF_INET to use IPv4
+  hints.ai_socktype = SOCK_DGRAM;
+  hints.ai_flags = AI_PASSIVE; // use own IP
+
+  if ((rv = getaddrinfo(NULL, port, &hints, &servinfo)) != 0) {
+    std::cerr << "getaddrinfo for recv: " << gai_strerror(rv) << std::endl;
+    return -1;
+  }
+
+  // loop through all the results and bind to the first we can
+  for(p = servinfo; p != NULL; p = p->ai_next) {
+    if ((the_sock = socket(p->ai_family, p->ai_socktype,
+			 p->ai_protocol)) == -1) {
+      std::cerr << "recv socket: " << std::strerror(errno) << std::endl;
+      continue;
+    }
+
+    if (bind(the_sock, p->ai_addr, p->ai_addrlen) == -1) {
+      close(the_sock);
+      std::cerr << "recv socket bind: " << std::strerror(errno) << std::endl;
+      continue;
+    }
+    
+    break;
+  }
+
+  if (p == NULL) {
+    std::cerr << "recv socket: failed to find a suitable network" << std::endl;
+    return -1;
+  }
+
+  freeaddrinfo(servinfo);
+  return the_sock;
+}
+
+
+// creates the socket for sending, using family to identify the protocol and port to send to
+// family = AF_INET or AF_INET6 or AF_UNSPEC
+// port = string representing the port number we send to
+// dest_host = string representing the destination address or name
+// return
+//   -1 on error
+//   the socked descriptor if OK
+int create_sock_send(int family, const char * port, const char * dest_host) {
+  int sockfd;
+  struct addrinfo hints, *servinfo, *p;
+  int rv;
+
+  memset(&hints, 0, sizeof hints);
+  hints.ai_family = family; // set to AF_INET to use IPv4
+  hints.ai_socktype = SOCK_DGRAM;
+
+  if ((rv = getaddrinfo(dest_host, port, &hints, &servinfo)) != 0) {
+    std::cerr << "getaddrinfo: " << gai_strerror(rv) << std::endl;
+    return -1;
+  }
+
+  // loop through all the results and make a socket
+  for(p = servinfo; p != NULL; p = p->ai_next) {
+    if ((sockfd = socket(p->ai_family, p->ai_socktype,
+			 p->ai_protocol)) == -1) {
+      std::cerr << "send socket: " << std::strerror(errno) << std::endl;
+      continue;
+    }
+    
+    break;
+  }
+
+  if (p == NULL) {
+    std::cerr << "send socket: failed to create" << std::endl;
+    return -1;
+  }
+
+  // connect, save the destination address with the socket so we can use send instead of sendto
+  if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+    std::cerr << "Send socket connect: " << std::strerror(errno) << std::endl;
+    return -1;
+  }
+
+  return sockfd;
+}
+
+
+// ********** the workers ***********
+// the sender thread;
+// sockfd: socket opened for sending the datagrams
+void send_thread(int sockfd) {
+  int numbytes;
+  char buf[MAXBUFLEN];
+  // create a mutex and a lock for the condition variable
+  std::mutex mtx;
+  std::unique_lock<std::mutex> lock(mtx);
+
+  while (1) {
+    // send then wait on cv
+    buf[0] = gl.value;
+    if (send(sockfd, buf, 1, 0) != 1) {
+      std::cerr << "Sender thread: " << std::strerror(errno) << std::endl;
+    }
+
+    if (gl.cv_timer.wait_for(lock, std::chrono::milliseconds(3000)) == std::cv_status::timeout) {
+      // we can do something here in case our sleep was interrupted; for this problem, we don't need to do anything
+    }
+    else {
+      // we can do something in case the timer expired. Nothing for us now.
+    }
+  }
+}
+
+// the receiver thread;
+// sockfd: socket opened for receiving the datagrams
+void recv_thread(int sockfd) {
+  char s[INET6_ADDRSTRLEN];
+  socklen_t addr_len;
+  int numbytes;
+  struct sockaddr_storage their_addr;
+  char buf[MAXBUFLEN];
+
+  addr_len = sizeof(their_addr);
+
+  while (1) {
+    if ((numbytes = recvfrom(sockfd, buf, MAXBUFLEN-1 , 0,
+			     (struct sockaddr *)&their_addr, &addr_len)) == -1) {
+      std::cerr << "receiver: " << std::strerror(errno) << std::endl;
+    }
+
+    std::cout << "receiver: " << buf[0] << " from " <<
+      inet_ntop(their_addr.ss_family,
+		get_in_addr((struct sockaddr *)&their_addr),
+		s, sizeof s) << std::endl;
+
+    // modify the global value
+    // should protect next assignment with a lock, but the other thread only reads, so no problem so far
+    gl.value = buf[0];
+
+    // wake up the sender
+    gl.cv_timer.notify_one();
+  }
+}
+
+
+// cmd line: bouncer destination
+int main(int argc, const char* argv[])
+{
+  int sock_send, sock_recv;  // one socket for sending, another for receiving
+
+  // are args OK?
+  if (argc != 2) {
+    std::cerr << "Usage: " << argv[0] << " destination_addr" << std::endl;
+    return 2;
+  }
+  
+  // args: family, port as string
+  if ((sock_recv = create_sock_recv(AF_INET, MYPORT)) < 0) {
+    std::cerr << "I failed" << std::endl;
+    return 1;
+  }
+
+  // args: family, port as string, destination name or addr as string
+  if ((sock_send = create_sock_send(AF_INET, DESTPORT, argv[1])) < 0) {
+    std::cerr << "I failed" << std::endl;
+    return 1;
+  }
+  
+  // start the threads
+  std::thread tsender(send_thread, sock_send);
+  std::thread treceiver(recv_thread, sock_recv);
+
+  tsender.join();
+  treceiver.join();
+
+
+  close(sock_send);
+  close(sock_recv);
+
+  return 0;
+}
